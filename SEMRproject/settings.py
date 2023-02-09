e4bf8db1af452b8314ef8a06cb2b8a8fa21fa9a6
MananShukla <43486501+MananShukla@users.noreply.github.com>
2023-02-09 09:54:03 -0500

    Update settings.py

--- SEMRproject/settings.py
+++ SEMRproject/settings.py
@@ -46,10 +46,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+      #  'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+      # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 '''
     'default': {
